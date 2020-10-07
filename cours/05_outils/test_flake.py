1846ff60e197353f878814aede24d690ce6200d1
Yann <yann@yann.fr>
2020-10-07 12:23:36 +0200

    fichiers de test

---
+++ cours/05_outils/test_flake.py
@@ -0,0 +1,22 @@
+import math  # module pas utilisé
+
+
+def maFonction():
+    pass
+
+
+def maFonction():  # fonction définie deux fois
+    return 1
+
+
+3+4*2  # instruction sans effet: pas détectée
+z = 2  # variable inutilisée: pas détectée
+
+
+x = 3
+y = 10
+while (x < y):
+    x += 1
+
+if (x == y/2):
+    break  # break pas dans une structure de boucle
